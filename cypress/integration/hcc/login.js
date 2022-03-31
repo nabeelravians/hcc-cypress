c742cba83932235a617597a98dacc41a4e4358b6
Muhammad Nabeel Mahtab <mnabeel@NIS-PK503-MacBook-Pro.local>
2022-03-31 12:28:40 +0500

    Revert all previous changes

--- cypress/integration/hcc/login.js
+++ cypress/integration/hcc/login.js
@@ -4,44 +4,43 @@ import LoginPage from "../PageObjects/LoginPage";
 
 describe('Login',function()
 {
-    // it('Login with valid credentials',function()
-    // {
-    //     const lp= new LoginPage()
-    //     lp.visit()
-    //     lp.enterCredentials('mnabeel@nisum.com','nisum123')
-    //     lp.clickLogin() 
-    //     lp.verifyWelcomeText()
+    it('Login with valid credentials',function()
+    {
+        const lp= new LoginPage()
+        lp.visit()
+        lp.enterCredentials('mnabeel@nisum.com','nisum123')
+        lp.clickLogin() 
+        lp.verifyWelcomeText()
        
-    // })
+    })
 
      it('Verify Invalid Notification Appear with Invalid Username and Password',function()
     {
         const lp= new LoginPage()
         lp.visit()
-        cy.url().should('include', 'google')
-        // lp.enterCredentials('mnabeel@nisum.com','nisum2123')
-        // lp.clickLogin() 
-        // lp.verifyInvalidLoginNotification()
+        lp.enterCredentials('mnabeel@nisum.com','nisum2123')
+        lp.clickLogin() 
+        lp.verifyInvalidLoginNotification()
        
     })
-    // it('Verify Disable or User Without any role should get Appropriate message',function()
-    // {
-    //     const lp= new LoginPage()
-    //     lp.visit()
-    //     lp.enterCredentials('nabeelravians@gmail.com','nisum123')
-    //     lp.clickLogin() 
-    //     lp.verifyNoUserRoleNotification()
+    it('Verify Disable or User Without any role should get Appropriate message',function()
+    {
+        const lp= new LoginPage()
+        lp.visit()
+        lp.enterCredentials('nabeelravians@gmail.com','nisum123')
+        lp.clickLogin() 
+        lp.verifyNoUserRoleNotification()
         
-    // })
-    // it('Verify User should logout succefully',function()
-    // {
-    //     const lp= new LoginPage()
-    //     lp.visit()
-    //     lp.enterCredentials('mnabeel@nisum.com','nisum123')
-    //     lp.clickLogin() 
-    //     lp.clickDownArrow()
-    //     lp.clickLogoutButton()
-    //     lp.verifyLogout()
+    })
+    it('Verify User should logout succefully',function()
+    {
+        const lp= new LoginPage()
+        lp.visit()
+        lp.enterCredentials('mnabeel@nisum.com','nisum123')
+        lp.clickLogin() 
+        lp.clickDownArrow()
+        lp.clickLogoutButton()
+        lp.verifyLogout()
         
-    // })
+    })
 })
